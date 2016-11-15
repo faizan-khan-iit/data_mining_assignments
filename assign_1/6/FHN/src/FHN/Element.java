df9911754ea2bf8eaf3c6b261ff59d8d00df41f3
faizan <faizan.khan.iitbhu@gmail.com>
2016-11-15 10:39:38 +0530

    add assignments

---
+++ assign_1/6/FHN/src/FHN/Element.java
@@ -0,0 +1,31 @@
+/*
+ * This class represents an Element of a utility list as used by the FHN algorithm.
+ */
+
+package FHN;
+
+public class Element 
+{
+	/* transaction id */
+	final int tid ;   
+	/* itemset utility */
+	final int iutils;   
+	/* remaining utility */
+	int rutils; 
+	public final int inutils;  	
+	
+	/*
+	 * Constructor. 
+	 * @param tid  the transaction id
+	 * @param iutils  the itemset utility (for positive items)
+	 * @param inutils  the itemset utility (for negative items)
+	 * @param rutils  the remaining utility
+	 */
+	
+	public Element(int tid, int iutils, int rutils, int inutils){
+		this.tid = tid;
+		this.iutils = iutils;
+		this.rutils = rutils;
+		this.inutils = inutils;
+	}	
+}
